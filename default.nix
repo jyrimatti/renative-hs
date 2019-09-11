7de944672c982156b7567d7f564dc4018f37004f
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2019-09-12 00:25:16 +0300

    cleanup default.nix

--- default.nix
+++ default.nix
@@ -20,15 +20,17 @@ let
     });
   });
 
-  ghcjs-base-stub-forked = { fetchgit, stdenv, mkDerivation,
-        aeson, attoparsec, containers, deepseq, ghc-prim, primitive, scientific, text, transformers, unordered-containers, vector
-     }:
+  ghcjs-base-stub-forked = { fetchgit, stdenv, mkDerivation, aeson, attoparsec, containers, deepseq, ghc-prim, primitive, scientific, text, transformers, unordered-containers, vector }:
      mkDerivation {
        pname = "ghcjs-base-stub";
        version = "0.2.0.0";
-       src = ../ghcjs-base-stub/.;
+       src = fetchgit {
+         url = "https://github.com/jyrimatti/ghcjs-base-stub.git";
+         sha256 = "0kzx8y7qj8afp6z5jyd9znbc6ivxwjqg23h7ngn5jd6pq4s5p65m";
+         rev = "207aa99b743a760a109421bc523cd6e4f9f96ce1";
+       };
        libraryHaskellDepends = [
-        aeson attoparsec containers deepseq ghc-prim primitive scientific text transformers unordered-containers vector
+         aeson attoparsec containers deepseq ghc-prim primitive scientific text transformers unordered-containers vector
        ];
        buildDepends = [];
        license = stdenv.lib.licenses.bsd3;
@@ -39,9 +41,7 @@ let
               #else haskellPackages.ghcjs-base-stub;
               else haskellPackages.callPackage ghcjs-base-stub-forked {};
 
-  myproject = { mkDerivation, base, deepseq, ghcjs-base, react-hs, react-native-hs, stdenv, nodejs, z3,
-        text, time, transformers, containers, network-uri, ghcjs-fetch, geojson, bytestring, generic-data
-      }:
+  myproject = { mkDerivation, base, deepseq, ghcjs-base, react-hs, react-native-hs, stdenv, nodejs, z3, text, time, transformers, containers, network-uri, ghcjs-fetch, geojson, bytestring, generic-data }:
       mkDerivation {
         pname = "myproject";
         version = "0.1.0.0";
@@ -55,25 +55,23 @@ let
         license = stdenv.lib.licenses.mit;
       };
 
-  ghcjs-fetch-forked = { fetchgit, stdenv, mkDerivation,
-        aeson, bytestring, case-insensitive, http-types, ghcjs-base, QuickCheck, hspec, hspec-core
-     }:
+  ghcjs-fetch-forked = { fetchgit, stdenv, mkDerivation, aeson, bytestring, case-insensitive, http-types, ghcjs-base, QuickCheck, hspec, hspec-core }:
      mkDerivation {
        pname = "ghcjs-fetch";
        version = "0.1.0.1";
-       src = ../ghcjs-fetch/.;
+       src = fetchgit {
+         url = "https://github.com/jyrimatti/ghcjs-fetch.git";
+         sha256 = "0vdw70pzrkfs4a56xanmxci9m1hqrq6mqghw83f170ps94mhf9ab";
+         rev = "a190d0bc1d480f55a288c09c5cfb590f5967ab44";
+       };
        libraryHaskellDepends = [
-        aeson bytestring case-insensitive http-types ghcjs-base
+         aeson bytestring case-insensitive http-types ghcjs-base
        ];
        buildDepends = [ QuickCheck hspec hspec-core ];
        license = stdenv.lib.licenses.bsd3;
      };
 
-  # my fork, until it gets to Hackage
-  react-hs-forked = { fetchgit, stdenv, mkDerivation,
-        aeson, base, bytestring, ghcjs-base, mtl, string-conversions,
-        template-haskell, text, time, unordered-containers
-     }:
+  react-hs = { fetchgit, stdenv, mkDerivation, aeson, base, bytestring, ghcjs-base, mtl, string-conversions, template-haskell, text, time, unordered-containers }:
      mkDerivation {
        pname = "react-hs";
        version = "0.1.1";
@@ -92,9 +90,7 @@ let
        license = stdenv.lib.licenses.bsd3;
      };
 
-  react-native-hs-git = { fetchgit, mkDerivation, base, deepseq, ghcjs-base, react-hs, stdenv, nodejs,
-        text, time, transformers, containers, network-uri
-     }:
+  react-native-hs = { fetchgit, mkDerivation, base, deepseq, ghcjs-base, react-hs, stdenv, nodejs, text, time, transformers, containers, network-uri }:
      mkDerivation {
        pname = "react-native-hs";
        version = "0.1.1";
@@ -104,49 +100,42 @@ let
          rev = "73734c37ec787d3fd785ee4612675bb8956f3b46";
        };
        libraryHaskellDepends = [
-        react-hs text time transformers containers network-uri
+         react-hs text time transformers containers network-uri
        ];
        homepage = "https://github.com/jyrimatti/react-native-hs";
        description = "React-native support for react-hs";
        license = stdenv.lib.licenses.mit;
      };
 
-  react-native-hs-local = { mkDerivation, base, deepseq, ghcjs-base, react-hs, stdenv, nodejs,
-        text, time, transformers, containers, network-uri
-     }:
+  react-native-hs-local = { mkDerivation, base, deepseq, ghcjs-base, react-hs, stdenv, nodejs, text, time, transformers, containers, network-uri }:
      mkDerivation {
        pname = "react-native-hs";
        version = "0.1.1";
        src = ../react-native-hs/.;
        libraryHaskellDepends = [
-        react-hs text time transformers containers network-uri
+         react-hs text time transformers containers network-uri
        ];
        homepage = "https://github.com/jyrimatti/react-native-hs";
        description = "React-native support for react-hs";
        license = stdenv.lib.licenses.mit;
      };
 
-  react-hs-local = { mkDerivation, aeson, base, bytestring, ghcjs-base, mtl, stdenv, string-conversions, template-haskell, text, time, unordered-containers, base-compat-batteries
-     }:
+  react-hs-local = { mkDerivation, aeson, base, bytestring, ghcjs-base, mtl, stdenv, string-conversions, template-haskell, text, time, unordered-containers, base-compat-batteries }:
      mkDerivation {
        pname = "react-hs";
        version = "0.1.1";
        src = ../react-hs/react-hs/.;
        libraryHaskellDepends = [
-        aeson base bytestring ghcjs-base mtl string-conversions template-haskell text time unordered-containers base-compat-batteries
+         aeson base bytestring ghcjs-base mtl string-conversions template-haskell text time unordered-containers base-compat-batteries
        ];
        license = stdenv.lib.licenses.bsd3;
      };
 
-  #react-hs = haskellPackages.callPackage react-hs-local { ghcjs-base = ghcjsbase; };
-  react-hs = haskellPackages.callPackage react-hs-forked { ghcjs-base = ghcjsbase; };
-
-  #react-native-hs = haskellPackages.callPackage react-native-hs-local { react-hs = react-hs; ghcjs-base = ghcjsbase; };
-  react-native-hs = haskellPackages.callPackage react-native-hs-git { react-hs = react-hs; ghcjs-base = ghcjsbase; };
-
+  react_hs = haskellPackages.callPackage react-hs-local { ghcjs-base = ghcjsbase; }; 
+ 
   drv = haskellPackages.callPackage myproject {
-    react-hs = react-hs;
-    react-native-hs = react-native-hs;
+    react-hs = react_hs;
+    react-native-hs = haskellPackages.callPackage react-native-hs { react-hs = react_hs; ghcjs-base = ghcjsbase; };
     ghcjs-base = ghcjsbase;
     ghcjs-fetch = pkgs.haskell.lib.dontCheck(haskellPackages.callPackage ghcjs-fetch-forked { ghcjs-base = ghcjsbase; });
   };
